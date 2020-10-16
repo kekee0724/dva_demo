06b4280c7ea341792ce1340cd46109a4ad248218
huangb <androidleo@outlook.com>
2020-10-16 15:41:13 +0800

    fix bugs

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,12 +6,27 @@ const ProductList = ({onDelete, products}) => {
   const columns = [{
     title: 'Name',
     dataIndex: 'name',
-  },{
+  }, {
     title: 'Actions',
-    render: (text,record) => {
-      return(
-        <Popconfirm title=""
+    render: (text, record) => {
+      return (
+        <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
+          <Button>Delete</Button>
+        </Popconfirm>
       );
     },
   }];
-}
+  return (
+    <Table
+      dataSource={products}
+      columns={columns}
+    />
+  );
+};
+
+ProductList.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  products: PropTypes.array.isRequired,
+};
+
+export default ProductList;
